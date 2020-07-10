7e182704c561bd44e47e772f0984b98d3c21463a
Pamq25k <you@example.com>
2020-07-10 20:11:35 +0000

    my lord

---
+++ precache-manifest.a16f08e3f44ca9247cc0a5f7c0696720.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfce60933ae1cb96798da1e378119083",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e666bed65a643a4cd30",
+    "url": "/static/js/2.1227ce0c.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/static/js/2.1227ce0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a01dd0c92c3aa0336b41",
+    "url": "/static/js/main.bcf9099a.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
